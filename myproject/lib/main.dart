3315dc182a082196db07d93939d6bd46ae8b6395
Gabriel <gabriel.artioli@epiousion.com.br>
2022-09-27 20:32:07 -0300

    Finalizado básico

--- myproject/lib/main.dart
+++ myproject/lib/main.dart
@@ -5,47 +5,122 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18);
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
-  MyApp({super.key});
+class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random(); // Add this line.
     return MaterialApp(
       title: 'Gerador de Plavaras Aleatórias',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Gerador de Plavaras Aleatórias'),
+      theme:  ThemeData(          // Add the 5 lines from here...
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
         ),
-        body: Center(                          // Drop the const, and
-          //child: Text('Hello World'),        // Replace this text...
-          child: ListView.builder(
-                itemBuilder: (context, iterator){
-                  if(iterator.isOdd) return const Divider(
-                    thickness: 2,
-                  );
-                  final index = iterator ~/ 2;
-                  print("Index: ${index}");
-                  print("Iterator: ${iterator}");
+      ),
+      home: RandomWords(),
+    );
+  }
+}
 
-                  if(index >= _suggestions.length){
-                    print("Gerando novas palavras...");
-                    _suggestions.addAll(generateWordPairs().take(10));
-                  }
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
 
-                  return ListTile(
-                    title: Text(
-                      _suggestions[index].asPascalCase,
-                      style: _biggerFont,
-                    ),
-                  );
-                }
-          ),  // With this text.
-        ),
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+
+  final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  void _pushSaved() {
+    Navigator.of(context).push(MaterialPageRoute<void>(
+      builder: (context) {
+        final tiles = _saved.map(
+              (pair) {
+            return ListTile(
+              title: Text(
+                pair.asPascalCase,
+                style: _biggerFont,
+              ),
+            );
+          },
+        );
+        final divided = tiles.isNotEmpty
+            ? ListTile.divideTiles(
+          context: context,
+          tiles: tiles,
+        ).toList()
+            : <Widget>[];
+
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Saved Suggestions'),
+          ),
+          body: ListView(children: divided),
+        );
+      },
+    ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.list),
+            onPressed: _pushSaved,
+            tooltip: 'Saved Suggestions',
+          ),
+        ],
+      ),
+      body: Center(                          // Drop the const, and
+        //child: Text('Hello World'),        // Replace this text...
+        child: ListView.builder(
+            itemBuilder: (context, iterator){
+              if(iterator.isOdd) {
+                return const Divider(
+                  thickness: 2,
+                );
+              }
+              final index = iterator ~/ 2;
+              if(index >= _suggestions.length){
+                _suggestions.addAll(generateWordPairs().take(10));
+              }
+              final alreadySaved = _saved.contains(_suggestions[index]);
+              return ListTile(
+                title: Text(
+                  _suggestions[index].asPascalCase,
+                  style: _biggerFont,
+                ),
+                trailing: Icon(
+                  alreadySaved ? Icons.favorite : Icons.favorite_border,
+                  color: alreadySaved ? Colors.redAccent : null,
+                  semanticLabel: alreadySaved ? "Remover" : "Adicionar",
+                ),
+                onTap: () {
+                  setState(() {
+                    if (alreadySaved) {
+                      _saved.remove(_suggestions[index]);
+                    } else {
+                      _saved.add(_suggestions[index]);
+                    }
+                  });
+                },
+              );
+            }
+        ),  // With this text.
       ),
     );
   }
